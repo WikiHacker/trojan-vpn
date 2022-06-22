c1047872d3c13b2cd9cd3f34ab41b53452612de9
fhmiseverando <107958850+fhmiseverando@users.noreply.github.com>
2022-06-22 23:19:54 +0700

    Add files via upload

---
+++ go.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# ==========================================
+bhoikfostyahya="raw.githubusercontent.com/fhmiseverando/trojan-vpn/main"
+wget -O add-tr https://${bhoikfostyahya}/xp.sh 
+wget -O add-tr https://${bhoikfostyahya}/vmessmenu.sh
+wget -O add-tr https://${bhoikfostyahya}/vlessmenu.sh
+wget -O add-tr https://${bhoikfostyahya}/user.sh
+wget -O add-tr https://${bhoikfostyahya}/trojanmenu.sh
+wget -O add-tr https://${bhoikfostyahya}/renew-vmess.sh
+wget -O add-tr https://${bhoikfostyahya}/renew-vless.sh
+wget -O add-tr https://${bhoikfostyahya}/renew-trojan-grpc.sh
+wget -O add-tr https://${bhoikfostyahya}/renew-trojan.sh
+wget -O add-tr https://${bhoikfostyahya}/menu.sh
+wget -O add-tr https://${bhoikfostyahya}/help.sh
+wget -O add-tr https://${bhoikfostyahya}/grpcmenu.sh
+wget -O add-tr https://${bhoikfostyahya}/del-vmess.sh
+wget -O add-tr https://${bhoikfostyahya}/del-vless.sh
+wget -O add-tr https://${bhoikfostyahya}/del-trojan-grpc.sh
+wget -O add-tr https://${bhoikfostyahya}/del-trojan.sh
+wget -O add-tr https://${bhoikfostyahya}/clear-log.sh
+wget -O add-tr https://${bhoikfostyahya}/cert.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-vmess.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-vless.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-service.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-trojan-grpc.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-trojan.sh
+wget -O add-tr https://${bhoikfostyahya}/add-vmess.sh
+wget -O add-tr https://${bhoikfostyahya}/add-vless.sh
+wget -O add-tr https://${bhoikfostyahya}/add-trojan-grpc.sh
+wget -O add-tr https://${bhoikfostyahya}/add-trojan.sh
