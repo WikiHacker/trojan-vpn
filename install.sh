dd4ee835235637ed4b444fc927e7acbc2f6c226b
fhmiseverando <107958850+fhmiseverando@users.noreply.github.com>
2022-06-22 23:30:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ mkdir /etc/nur
 touch /etc/nur/author
 read -p "(Silahkan masukan Nama anda) : " nama
 echo "${nama}" >> /etc/nur/author
+bhoikfostyahya="raw.githubusercontent.com/fhmiseverando/trojan-vpn/main"
 #instal core
 wget https://github.com/XTLS/Xray-install/raw/main/install-release.sh
 bash install-release.sh
@@ -335,34 +336,34 @@ systemctl enable vless-ws
 systemctl enable vmess-ws
 
 #copy ke /usr/bin
-cd /root
-cp xp.sh /usr/bin/xp
-cp vmessmenu.sh /usr/bin/vmessmenu
-cp vlessmenu.sh /usr/bin/vlessmenu
-cp user.sh /usr/bin/user
-cp trojanmenu.sh /usr/bin/trojanmenu
-cp renew-vmess.sh /usr/bin/renew-vmess
-cp renew-vless.sh /usr/bin/renew-vless
-cp renew-trojan-grpc.sh /usr/bin/renew-trojan-grpc
-cp renew-trojan.sh /usr/bin/renew-trojan
-cp menu.sh /usr/bin/menu
-cp help.sh /usr/bin/help
-cp grpcmenu.sh /usr/bin/grpcmenu
-cp del-vmess.sh /usr/bin/del-vmess
-cp del-vless.sh /usr/bin/del-vless
-cp del-trojan-grpc.sh /usr/bin/del-trojan-grpc
-cp del-trojan.sh /usr/bin/del-trojan
-cp clear-log.sh /usr/bin/clear-log
-cp cert.sh /usr/bin/cert
-cp cek-vmess.sh /usr/bin/cek-vmess
-cp cek-vless.sh /usr/bin/cek-vless
-cp cek-service.sh /usr/bin/cek-service
-cp cek-trojan-grpc.sh /usr/bin/cek-trojan-grpc
-cp cek-trojan.sh /usr/bin/cek-trojan
-cp add-vmess.sh /usr/bin/add-vmess
-cp add-vless.sh /usr/bin/add-vless
-cp add-trojan-grpc.sh /usr/bin/add-trojan-grpc
-cp add-trojan.sh /usr/bin/add-trojan
+cd /usr/bin
+wget -O add-tr https://${bhoikfostyahya}/xp.sh 
+wget -O add-tr https://${bhoikfostyahya}/vmessmenu.sh
+wget -O add-tr https://${bhoikfostyahya}/vlessmenu.sh
+wget -O add-tr https://${bhoikfostyahya}/user.sh
+wget -O add-tr https://${bhoikfostyahya}/trojanmenu.sh
+wget -O add-tr https://${bhoikfostyahya}/renew-vmess.sh
+wget -O add-tr https://${bhoikfostyahya}/renew-vless.sh
+wget -O add-tr https://${bhoikfostyahya}/renew-trojan-grpc.sh
+wget -O add-tr https://${bhoikfostyahya}/renew-trojan.sh
+wget -O add-tr https://${bhoikfostyahya}/menu.sh
+wget -O add-tr https://${bhoikfostyahya}/help.sh
+wget -O add-tr https://${bhoikfostyahya}/grpcmenu.sh
+wget -O add-tr https://${bhoikfostyahya}/del-vmess.sh
+wget -O add-tr https://${bhoikfostyahya}/del-vless.sh
+wget -O add-tr https://${bhoikfostyahya}/del-trojan-grpc.sh
+wget -O add-tr https://${bhoikfostyahya}/del-trojan.sh
+wget -O add-tr https://${bhoikfostyahya}/clear-log.sh
+wget -O add-tr https://${bhoikfostyahya}/cert.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-vmess.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-vless.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-service.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-trojan-grpc.sh
+wget -O add-tr https://${bhoikfostyahya}/cek-trojan.sh
+wget -O add-tr https://${bhoikfostyahya}/add-vmess.sh
+wget -O add-tr https://${bhoikfostyahya}/add-vless.sh
+wget -O add-tr https://${bhoikfostyahya}/add-trojan-grpc.sh
+wget -O add-tr https://${bhoikfostyahya}/add-trojan.sh
 cd /usr/bin
 chmod +x xp
 chmod +x vmessmenu
